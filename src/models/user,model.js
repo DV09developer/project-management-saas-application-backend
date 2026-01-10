888a783bb7ff2015a8fc4656f4f633a0eb70257b
DV09developer <divyeshvoriya3@gmail.com>
2026-01-10 22:56:46 +0530

    add usermodel in details

--- src/models/user,model.js
+++ src/models/user,model.js
@@ -1,13 +1,39 @@
 import mongoose , { Schema } from "mongoose";
 
 const userSchema = new Schema({
-    username : {
+    username: {
         type: String,
         required: true,
         trim: true,
         unique: true,
         lowercase: true,
         index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        index: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstname: {
+        type: String,
+        trim: true,
+    },
+    lastname: {
+        type: String,
+        trim: true
+    },
+    avatar: {
+        type: String,
+    },
+    refreshToken: {
+        type: String,
     }
 }, {timestamps : true})
 
